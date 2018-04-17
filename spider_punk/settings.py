6204536c8e99578d001ef51f30ea26e46e9ba9d7
rohit keshav <rohittk239@gmail.com>
2018-04-16 22:54:39 -0400

    works

---
+++ spider_punk/settings.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for spider_punk project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+import os
+BOT_NAME = 'spider_punk'
+
+SPIDER_MODULES = ['spider_punk.spiders']
+NEWSPIDER_MODULE = 'spider_punk.spiders'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+CHROME_DRIVER = BASE_DIR + '/chromedriver.exe'
+
+# TODO: later use
+SCRAPE_URLS = {
+    'JobSpider': 'https://www.federalreserve.gov/start-job-search.htm'
+}
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'spider_punk (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'spider_punk.middlewares.SpiderPunkSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'spider_punk.middlewares.SpiderPunkDownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'spider_punk.pipelines.SpiderPunkPipeline': 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+LOG_FILE = 'spider.log'
---
+++ spider_punk/spiders/job_spider.py
@@ -0,0 +1,66 @@
+import time
+from csv import DictWriter
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+from scrapy import Spider, Request
+
+
+class JobsSpider(Spider):
+    name = "job_search"
+
+    def __init__(self, keyword=None, category=None, **kwargs):
+
+        self.keyword = keyword
+        self.category = category
+
+        super().__init__(**kwargs)
+
+    def start_requests(self):
+
+        yield Request(url='https://www.federalreserve.gov/start-job-search.htm', callback=self.parse)
+
+    def parse(self, response):
+
+        driver = self.job_search_help('https://www.federalreserve.gov/start-job-search.htm')
+
+        with open("jobs.html", "w+") as h_blob:
+            h_blob.write(driver.page_source)
+
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        per_job_data = soup.find(id='requisitionListInterface.listRequisition').tbody.find_all('div', attrs={'class': 'iconcontentpanel'})
+
+        retval = []
+        headers = ['role', 'location', 'posting_date']
+
+        for j_obj in per_job_data:
+            retval.append(dict(zip(headers, [data.get_text() for data in j_obj.find_all('div', attrs={'class': 'contentlinepanel'})])))
+
+        save_as_csv(retval, headers, 'jobs')
+
+    def job_search_help(self, base_url):
+        driver = webdriver.Chrome(executable_path='../../chromedriver')
+
+        driver.get(base_url)
+
+        driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+
+        if self.category is not None:
+            select = Select(driver.find_element_by_xpath('//*[@id="advancedSearchInterface.jobfield1L1"]'))
+            select.select_by_visible_text(self.category)
+
+        if self.keyword is not None:
+            driver.find_element_by_xpath('//*[@id="advancedSearchInterface.keywordInput"]').send_keys(self.keyword)
+
+        if self.keyword is not None or self.category is not None:
+            driver.find_element_by_xpath('//*[@id="advancedSearchFooterInterface.searchAction"]').click()
+            time.sleep(5)
+
+        return driver
+
+
+def save_as_csv(lod, headers, fname):
+    with open(fname + '.csv', 'w') as f:
+        fobj = DictWriter(f, fieldnames=headers)
+        fobj.writeheader()
+        fobj.writerows(lod)
