cf02850085b54e6bd6b462bd37d5c760bb63dfb8
rohit keshav <rohittk239@gmail.com>
2018-04-24 15:18:30 -0400

    to json function in utils

--- spider_punk/spiders/job_spider.py
+++ spider_punk/spiders/job_spider.py
@@ -7,6 +7,10 @@ from scrapy import Spider, Request
 
 
 class JobsSpider(Spider):
+    """
+        :keyword, category - pass param to keyword or select category as a search parameter
+    """
+
     name = "job_search"
 
     def __init__(self, keyword=None, category=None, **kwargs):
@@ -17,10 +21,19 @@ class JobsSpider(Spider):
         super().__init__(**kwargs)
 
     def start_requests(self):
+        """
+        :return: Request object that is used in Parse,
+                 inherited from Spider class
+        """
 
         yield Request(url='https://www.federalreserve.gov/start-job-search.htm', callback=self.parse)
 
     def parse(self, response):
+        """
+        Parses page to get relevant data out, using selenium, beautiful soup also used,
+        since page elements seem to unreliable, post change in DOM. Hence loading it into a BeautifulSoup
+        to get relevant data out.
+        """
 
         driver = self.job_search_help('https://www.federalreserve.gov/start-job-search.htm')
 
@@ -30,7 +43,7 @@ class JobsSpider(Spider):
         soup = BeautifulSoup(driver.page_source, "lxml")
         per_job_data = soup.find(id='requisitionListInterface.listRequisition').tbody.find_all('div', attrs={'class': 'iconcontentpanel'})
 
-        retval = []
+        retval = list()
         headers = ['role', 'location', 'posting_date']
 
         for j_obj in per_job_data:
@@ -39,6 +52,12 @@ class JobsSpider(Spider):
         utils.save_as_csv(retval, headers, 'jobs')
 
     def job_search_help(self, base_url):
+        """
+        :param base_url: url of page to be processed
+        :return: driver object back to parse function
+        Implements the search parameters, if any using selenium on the page.
+        """
+
         driver = webdriver.Chrome(executable_path='../../chromedriver')
 
         driver.get(base_url)
--- spider_punk/utils.py
+++ spider_punk/utils.py
@@ -1,8 +1,39 @@
+"""
+    Module, for all utilities required for the project
+"""
+
 import csv
+import json
 
 
 def save_as_csv(lod, headers, fname):
+    """
+    :param lod: list of dictionaries, containing row wise data, key being column,
+                value being the relevant data in the for that row
+    :param headers: column names
+    :param fname: file name of the csv
+    """
     with open(fname + '.csv', 'w') as f:
         fobj = csv.DictWriter(f, fieldnames=headers)
         fobj.writeheader()
         fobj.writerows(lod)
+
+
+def save_as_json(lod, fname, pk=None):
+    """
+    :param pk: check if key values is to be used from the dict or not, else use auto incrementing numbers
+    :param lod: list of dictionaries to be saved as JSON
+    :param fname: file name of the JSON
+    """
+    rdict = dict()
+    anum = 1
+
+    for vdict in lod:
+        if pk is None:
+            rdict[anum] = vdict
+            anum += 1
+        else:
+            rdict[vdict.get(pk)] = vdict
+
+    with open(fname + '.json', 'w') as f:
+        json.dump(rdict, f)
